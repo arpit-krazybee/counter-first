d18b2f17d78e0b0e6ed0ab6ef23d42911619fa6d
arpit-krazybee <arpit.gaurav@krazybee.com>
2019-07-15 15:21:43 +0530

    Test React App

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,15 +1,38 @@
  import React, { Component } from 'react';
  class Counter extends Component {
      state = {  
-         count: 4
+         count:0,
+         tag: []
+         
      };
+     styles={
+        fontSize:10,
+        fontWeight:'bold'
+     };
+     renderTags(){
+         if(this.state.tag.length===0) return<p>there are no elements</p>;
+         return  <ul>{this.state.tag.map(tags => <li key={tags}>{tags}</li>)} </ul>;
+     }
      render() { 
+        let classes = this.getBadgeClasses();
          return(
-             <div> <h1>HEEEElllllllllllllllllllllllllllllllllllllllloooooooooooooooooooooooooooooooo</h1>
-             <span>{this.state.count}</span>
-             <button>Increment</button>
+             <div> <h3>Heellllloooooooo</h3>
+             <span style={ this.styles} className={this.getBadgeClasses()}>{this.formatCount()}</span>
+             <button className="btn btn-secondary btn-sm">Increment</button>
+            {this.renderTags()}
              </div>
          );
+     }
+     getBadgeClasses() {
+         let classes = "badge m2 badge-";
+         classes += (this.state.count === 0) ? "warning" : "primary";
+         return classes;
+     }
+
+     formatCount()
+     {
+         const { count }=this.state;
+         return count === 0 ? 'Zero': count;
      }
  }
   
